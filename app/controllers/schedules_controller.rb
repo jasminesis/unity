d42f8964c89646460780b135ce8e82f8817d5301
jasminesis <jasmine-aips@hotmail.com>
2019-11-11 17:31:44 +0800

    calendar displays

---
+++ app/controllers/schedules_controller.rb
@@ -0,0 +1,18 @@
+class SchedulesController < ApplicationController
+  def index
+    @schedules = Schedule.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_11_063238) do
+ActiveRecord::Schema.define(version: 2019_11_11_080758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 2019_11_11_063238) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_friends_on_user_id"
+  end
+
+  create_table "schedules", force: :cascade do |t|
+    t.string "event"
+    t.datetime "start_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "user_games", force: :cascade do |t|
