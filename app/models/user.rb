7eb7d9beccf0e4bbe9d5e8037515647fb487559b
jasminesis <jasmine-aips@hotmail.com>
2019-11-12 12:05:11 +0800

    1:many user to schedules

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :schedules
+  has_many :schedules
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_11_101223) do
+ActiveRecord::Schema.define(version: 2019_11_12_040343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2019_11_11_101223) do
     t.datetime "start_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_schedules_on_user_id"
   end
 
   create_table "schedules_users", force: :cascade do |t|
@@ -77,5 +79,6 @@ ActiveRecord::Schema.define(version: 2019_11_11_101223) do
   end
 
   add_foreign_key "friends", "users"
+  add_foreign_key "schedules", "users"
   add_foreign_key "user_games", "users"
 end
