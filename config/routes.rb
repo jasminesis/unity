b5500097bc34590ce97daa27d9a0c0b4b2295242
edenlim <edenlim123@gmail.com>
2019-11-12 10:17:13 +0800

    Able to update online status when logged in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'user/sessions' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'users#index'
+  get '/users' => 'users#index', as: 'users'
+  get '/users/new' => 'users#new', as: 'new_user'
+  post '/users' => 'users#create'
+  get '/users/:id' => 'users#show' , as: 'user'
+  get '/users/:id/edit' => 'users#edit', as: 'edit_user'
+  patch '/users/:id' => 'users#update'
+  delete '/users/:id' => 'users#destroy'
 end
