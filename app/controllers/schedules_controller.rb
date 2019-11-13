52a6116b27bdedb4c40f1a2cff7a07d7886b85ee
jasminesis <jasmine-aips@hotmail.com>
2019-11-13 17:08:57 +0800

    can add new event with friend!!!

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -5,7 +5,15 @@ class SchedulesController < ApplicationController
   end
 
   def new
+    if params[:user_id]
+      puts "awesome"
+    end
 
+    @current_id = current_user.id
+    @friend_a = Friend.where(:user_id => current_user.id).map{|x|x.friend_user_id}
+    @friend_b = Friend.where(:friend_user_id => current_user.id).map{|x|x.user_id}
+    @list = @friend_a.concat(@friend_b)
+    @friends = User.where("id IN (?)",@list)
   end
 
   def create
@@ -13,10 +21,10 @@ class SchedulesController < ApplicationController
       
     @schedule.user = current_user
   
-    if @schedule.save!
+    if @schedule.save
       redirect_to @schedule
-    # else
-      # render plain: "Failed"
+    else
+      render plain: schedule_params
       # render 'new'
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -14,13 +14,10 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homes#index'
-  get '/users' => 'users#index', as: 'users'
-  get '/users/new' => 'users#new', as: 'new_user'
-  post '/users' => 'users#create'
-  get '/users/:id' => 'users#show' , as: 'user'
-  get '/users/:id/edit' => 'users#edit', as: 'edit_user'
-  patch '/users/:id' => 'users#update'
-  delete '/users/:id' => 'users#destroy'
+
+  resources :users do
+    resources :schedules, only: [:new]
+  end
 
   get '/users/add/:id' => 'friends#create'
   delete '/users/delete/:id' => 'friends#destroy'
