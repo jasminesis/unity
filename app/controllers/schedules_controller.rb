e052e5303cd8e9c9b74e3a09105e391a4ce7ff16
edenlim <edenlim123@gmail.com>
2019-11-14 15:16:24 +0800

    Remove duplicate code

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -23,7 +23,7 @@ class SchedulesController < ApplicationController
     @schedule = Schedule.new(schedule_params)
 
     @schedule.user = current_user
-    
+
     if @schedule.save
 
       redirect_to @schedule
@@ -39,28 +39,6 @@ class SchedulesController < ApplicationController
   end
 
 
-  def update
-    @schedule = Schedule.new(schedule_params)
-
-    @schedule.user = current_user
-    @schedule_id = params[:id]
-    
-
-    if @schedule.save
-
-      redirect_to @schedule
-    else
-      # render plain: schedule_params
-      # @schedule = Schedule.find(params[:id])
-      @current_id = current_user.id
-      @friend_a = Friend.where(:user_id => current_user.id).map{|x|x.friend_user_id}
-      @friend_b = Friend.where(:friend_user_id => current_user.id).map{|x|x.user_id}
-      @list = @friend_a.concat(@friend_b)
-      @friends = User.where("id IN (?)",@list)
-      render "edit"
-    end
-  end
-
   def show
     @schedule = Schedule.find(params[:id])
   end
@@ -73,7 +51,7 @@ class SchedulesController < ApplicationController
     @friend_b = Friend.where(:friend_user_id => current_user.id).map{|x|x.user_id}
     @list = @friend_a.concat(@friend_b)
     @friends = User.where("id IN (?)",@list)
-    
+
   end
 
   def update
@@ -81,7 +59,7 @@ class SchedulesController < ApplicationController
 
     @schedule.user = current_user
     @schedule_id = params[:id]
-    
+
     if @schedule.save
 
       redirect_to @schedule
