c76ff661c6a9ebfb2534a535e48612bb32ca4c1b
jasminesis <jasmine-aips@hotmail.com>
2019-11-14 13:44:44 +0800

    can query to go to new date for free time

--- app/controllers/freetimes_controller.rb
+++ app/controllers/freetimes_controller.rb
@@ -1,30 +1,33 @@
+# frozen_string_literal: true
+
 class FreetimesController < ApplicationController
   def index
-    @freetimes = Freetime.all
-
+    @freetimes = Freetime.where("user_id = #{current_user.id}")
   end
 
   def new
-
-  end
+    if params[:newdate]
+      @datetime = DateTime.parse(params[:newdate])
+    else      
+      @datetime = DateTime.now
+    end
+   end
 
   def create
-
-    params[:freetime][:time].each do|time| 
-      something = Freetime.new(:time => time, :user_id => current_user.id)
+    params[:freetime][:time].each do |time|
+      something = Freetime.new(time: time, user_id: current_user.id)
       something.save!
-     end
+    end
 
-     redirect_to freetimes_path
+    redirect_to freetimes_path
     # if @freetime.save!
-      # render plain: freetime_params
+    # render plain: freetime_params
     # else
-      # render plain: "Failed"
+    # render plain: "Failed"
     # end
   end
 
-  def show
-  end
+  def show; end
 
   def edit
     @freetime = Freetime.find(params[:id])
