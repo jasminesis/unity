d2239edc05c665590a9aa0453799a61bfba7fe68
David Moskowitz <5844023+david-sg@users.noreply.github.com>
2019-11-14 14:42:30 +0800

    Schedule validations (#46)
    * add validation check to new and edit
    
    * display error message, output proper destroy link
    
    * add error message if event in new and edit

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,4 +1,7 @@
 class SchedulesController < ApplicationController
+
+ 
+
   def index
 
     @schedules = Schedule.where("user_id = #{current_user.id}")
@@ -22,14 +25,41 @@ class SchedulesController < ApplicationController
     @schedule = Schedule.new(schedule_params)
 
     @schedule.user = current_user
+    
+    if @schedule.save
 
+      redirect_to @schedule
+    else
+      # render plain: schedule_params
+      @current_id = current_user.id
+      @friend_a = Friend.where(:user_id => current_user.id).map{|x|x.friend_user_id}
+      @friend_b = Friend.where(:friend_user_id => current_user.id).map{|x|x.user_id}
+      @list = @friend_a.concat(@friend_b)
+      @friends = User.where("id IN (?)",@list)
+      render "new"
+    end
+  end
+
+
+  def update
+    @schedule = Schedule.new(schedule_params)
+
+    @schedule.user = current_user
+    @schedule_id = params[:id]
+    
 
     if @schedule.save
 
       redirect_to @schedule
     else
-      render plain: schedule_params
-      # render 'new'
+      # render plain: schedule_params
+      # @schedule = Schedule.find(params[:id])
+      @current_id = current_user.id
+      @friend_a = Friend.where(:user_id => current_user.id).map{|x|x.friend_user_id}
+      @friend_b = Friend.where(:friend_user_id => current_user.id).map{|x|x.user_id}
+      @list = @friend_a.concat(@friend_b)
+      @friends = User.where("id IN (?)",@list)
+      render "edit"
     end
   end
 
@@ -39,11 +69,13 @@ class SchedulesController < ApplicationController
 
   def edit
     @schedule = Schedule.find(params[:id])
+    @schedule_id = params[:id]
     @current_id = current_user.id
     @friend_a = Friend.where(:user_id => current_user.id).map{|x|x.friend_user_id}
     @friend_b = Friend.where(:friend_user_id => current_user.id).map{|x|x.user_id}
     @list = @friend_a.concat(@friend_b)
     @friends = User.where("id IN (?)",@list)
+    
   end
 
   def update
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,5 +1,5 @@
 class Schedule < ApplicationRecord
     has_and_belongs_to_many :users
     belongs_to :user
-
+    validates :event, presence: true, length: { minimum: 1, maximum: 80 }
 end
