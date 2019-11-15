a0478eca4cc8fd13c5216a643658c540a8a2a64a
edenlim <edenlim123@gmail.com>
2019-11-15 17:19:14 +0800

    Search function added for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,18 +2,34 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @games = Game.all
-    @users = User.where.not(id: current_user.id)
-    @mygames = GamesUser.where(user_id: current_user.id).pluck(:game_id)
-    #find all my friends!
-    if user_signed_in?
-      # # get all your friends in an array
-      @friend_a = Friend.where(:user_id => current_user.id).map{|x|x.friend_user_id}
-      @friend_b = Friend.where(:friend_user_id => current_user.id).map{|x|x.user_id}
-      @list = @friend_a.concat(@friend_b)
-      @friends = User.where("id IN (?)",@list)
+    @params = params[:search]
+    if @params
+      @games = Game.all
+      # @users = User.where(:username => @params)
+      # @users = User.search(params[:search])
+      @users = User.where(["username LIKE ?", "%#{params[:search]}%"])
+      @mygames = GamesUser.where(user_id: current_user.id).pluck(:game_id)
+      #find all my friends!
+      if user_signed_in?
+        # # get all your friends in an array
+        @friend_a = Friend.where(:user_id => current_user.id).map{|x|x.friend_user_id}
+        @friend_b = Friend.where(:friend_user_id => current_user.id).map{|x|x.user_id}
+        @list = @friend_a.concat(@friend_b)
+        @friends = User.where("id IN (?)",@list)
       end
-
+    else
+      @games = Game.all
+      @users = User.where.not(id: current_user.id)
+      @mygames = GamesUser.where(user_id: current_user.id).pluck(:game_id)
+      #find all my friends!
+      if user_signed_in?
+        # # get all your friends in an array
+        @friend_a = Friend.where(:user_id => current_user.id).map{|x|x.friend_user_id}
+        @friend_b = Friend.where(:friend_user_id => current_user.id).map{|x|x.user_id}
+        @list = @friend_a.concat(@friend_b)
+        @friends = User.where("id IN (?)",@list)
+      end
+    end
   end
 
   def show
