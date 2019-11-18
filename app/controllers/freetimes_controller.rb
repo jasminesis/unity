0bf04915a63eabb8f4fbf997ea77f34ad17c3474
David M <davidmosk@gmail.com>
2019-11-18 10:11:16 +0800

    validate for existing freetimes

--- app/controllers/freetimes_controller.rb
+++ app/controllers/freetimes_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class FreetimesController < ApplicationController
+
   def index
     @freetimes = Freetime.where("user_id = #{current_user.id}").order('time ASC')
   end
@@ -19,17 +20,41 @@ class FreetimesController < ApplicationController
    end
 
   def create
+    isduplicate = false
     params[:freetime][:time].each do |time|
       something = Freetime.new(time: DateTime.parse(time), user_id: current_user.id)
-      something.save!
-    end
+      # something.save!
 
-    redirect_to freetimes_path
-    # if @freetime.save!
-    # render plain: freetime_params
-    # else
-    # render plain: "Failed"
-    # end
+
+      if something.save
+        
+      else
+        isduplicate = true
+        # render plain: schedule_params
+        if params[:counter] == "increase"
+          session[:freetime_counter] = session[:freetime_counter] + 1
+          @datetime = DateTime.current.beginning_of_day().advance(:days => session[:freetime_counter])
+
+
+        elsif params[:counter] == "decrease"
+          session[:freetime_counter] = session[:freetime_counter] - 1
+          @datetime = DateTime.current.beginning_of_day().advance(:days => session[:freetime_counter])
+
+        else
+          session[:freetime_counter] = 0
+          @datetime = DateTime.current
+
+        end 
+        
+      end
+
+
+    end
+    if isduplicate == true 
+      render "new"
+    else 
+      redirect_to freetimes_path
+    end
   end
 
   def show; end
