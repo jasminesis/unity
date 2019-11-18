0157426f54f88483115e24e538e68aaa3b2864ea
jasminesis <jasmine-aips@hotmail.com>
2019-11-19 07:30:04 +0800

    commented line

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ApplicationRecord
     # has_many :games_user
     has_and_belongs_to_many :users
-    validates_uniqueness_of :user_id, scope: :game_id, :message => "Already Fav'd"
+    # validates_uniqueness_of :user_id, scope: :game_id, :message => "Already Fav'd"
 end
 
