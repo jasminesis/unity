fecee727ac7d8c7289db142a9818993641701336
David M <davidmosk@gmail.com>
2019-11-18 13:28:14 +0800

    validate schedule for already taken time

--- app/models/freetime.rb
+++ app/models/freetime.rb
@@ -1,4 +1,4 @@
 class Freetime < ApplicationRecord
     belongs_to :user
-    validates_uniqueness_of :time, scope: :user_id
+    validates_uniqueness_of :time, scope: :user_id, :message => "already taken"
 end
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -3,4 +3,7 @@ class Schedule < ApplicationRecord
     belongs_to :user
     has_many :schedules_user
     validates :event, presence: true, length: { minimum: 1, maximum: 80 }
+    validates_uniqueness_of :start_time, scope: :user_id, :message => "Already booked"
+   
 end
+
