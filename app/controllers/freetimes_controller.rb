02711ea8b6cf659ca13d7152b2a263640aab58b8
David Moskowitz <5844023+david-sg@users.noreply.github.com>
2019-11-18 11:21:07 +0800

    Prevent duplicate freetimes (#82)
    * validate for existing freetimes
    
    * check if user has existing freetime, dont display if already have freetime chosen

--- app/controllers/freetimes_controller.rb
+++ app/controllers/freetimes_controller.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
 class FreetimesController < ApplicationController
+
   def index
     @freetimes = Freetime.where("user_id = #{current_user.id}").order('time ASC')
   end
 
   def new
+    @userfreetimes = Freetime.where("user_id= #{current_user.id}").pluck(:time)
+
     if params[:counter] == "increase"
       session[:freetime_counter] = session[:freetime_counter] + 1
       @datetime = DateTime.current.beginning_of_day().advance(:days => session[:freetime_counter])
@@ -19,17 +22,42 @@ class FreetimesController < ApplicationController
    end
 
   def create
-    params[:freetime][:time].each do |time|
+    isduplicate = false
+    if params[:freetime].blank?
+      puts "blank"
+      isduplicate = true
+      else 
+      puts "not blank"
+      params[:freetime][:time].each do |time|
       something = Freetime.new(time: DateTime.parse(time), user_id: current_user.id)
-      something.save!
+        
+        if something.save
+        
+          else
+          isduplicate = true
+        end
     end
+  
+  end
 
-    redirect_to freetimes_path
-    # if @freetime.save!
-    # render plain: freetime_params
-    # else
-    # render plain: "Failed"
-    # end
+    if isduplicate == true 
+       # render plain: schedule_params
+      if params[:counter] == "increase"
+        session[:freetime_counter] = session[:freetime_counter] + 1
+        @datetime = DateTime.current.beginning_of_day().advance(:days => session[:freetime_counter])
+        elsif params[:counter] == "decrease"
+        session[:freetime_counter] = session[:freetime_counter] - 1
+        @datetime = DateTime.current.beginning_of_day().advance(:days => session[:freetime_counter])
+
+        else
+        session[:freetime_counter] = 0
+        @datetime = DateTime.current
+
+      end 
+      render "new"
+    else 
+      redirect_to freetimes_path
+    end
   end
 
   def show; end
